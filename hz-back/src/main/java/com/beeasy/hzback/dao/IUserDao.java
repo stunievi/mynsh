850ce38b46b7192a0a1e25052898132121e82f7f
llyb120 <llyb120@163.com>
2017-12-28 13:11:38 +0800

    fuck

--- hz-back/src/main/java/com/beeasy/hzback/dao/IUserDao.java
+++ hz-back/src/main/java/com/beeasy/hzback/dao/IUserDao.java
@@ -1,12 +1,4 @@
 package com.beeasy.hzback.dao;
 
-import com.beeasy.hzback.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserDao extends JpaRepository<User,Long> {
-    User findByName(String userName);
-//    User findByUserName(String userName);
-//    User findByUserNameOrEmail(String username, String email);
+public interface IUserDao {
 }
-
-
