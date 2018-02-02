f7a84a2433510d199bc2ae3f6f3c0ab50378fc12
company_bin <tiemaocsdn@qq.com>
2018-02-02 15:48:15 +0800

    增加日志输出的配置

--- hz-back/src/main/java/com/beeasy/hzback/modules/setting/controller/index.java
+++ hz-back/src/main/java/com/beeasy/hzback/modules/setting/controller/index.java
@@ -1,12 +1,15 @@
 package com.beeasy.hzback.modules.setting.controller;
 
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Slf4j
 @Controller
 @RequestMapping("/admin")
 public class index {
