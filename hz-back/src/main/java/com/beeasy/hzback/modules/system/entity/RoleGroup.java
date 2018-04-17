6fa5b3d0719a382cce5ee45a184b1f25d07068d7
bin <llyb120@163.com>
2018-04-17 13:40:15 +0800

    完善配置文件

---
+++ hz-back/src/main/java/com/beeasy/hzback/modules/system/entity/RoleGroup.java
@@ -0,0 +1,23 @@
+package com.beeasy.hzback.modules.system.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "t_role_group")
+public class RoleGroup {
+
+    @Id
+    @GeneratedValue
+    Long id;
+
+    @ManyToOne
+    User user;
+
+    String roleName;
+
+}
