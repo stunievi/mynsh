64174f5b645b6f020d55afe654c1011e5f5f789b
bin <llyb120@163.com>
2018-07-03 13:46:04 +0800

    增加角色的模糊查询

--- hz-back/src/main/java/com/beeasy/hzback/core/util/SqlUtils.java
+++ hz-back/src/main/java/com/beeasy/hzback/core/util/SqlUtils.java
@@ -83,7 +83,9 @@ public class SqlUtils {
     }
 
     public Page pageQuery(String sql, Pageable pageable){
-        String countSql = sql.replaceFirst("select(.+)from", "select count(*) as num from");
+        String countSql = sql
+                .replaceFirst("select(.+)from", "select count(*) as num from")
+                .replaceFirst("SELECT(.+)FROM","SELECT count(*) as num FROM");
         List<Map<String, String>> countList = query(countSql,null);
         int count = Integer.valueOf(countList.get(0).getOrDefault("num","0"));
         //添加分页
