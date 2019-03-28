14b456ff0501e2f15afa1b583525edc9926b25df
wangli94 <15271243280@163.com>
2019-03-28 08:53:51 +0800

    JSON keg值转换，6种方法

---
+++ load-qcc/src/main/java/com/beeasy/test/test2.java
@@ -0,0 +1,12 @@
+package com.beeasy.test;
+
+import com.beeasy.loadqcc.service.Transition;
+
+public class test2 {
+    public static void main(String[] args){
+        Transition aaa=new Transition();
+       aaa.aa();
+
+       // aaa.bb();
+    }
+}
