9c00274418df7d5c4c82a0aaed31c9f05cd7fc22
llyb120 <llyb120@163.com>
2019-07-24 15:06:43 +0800

    工作流配置

--- hz-bpm/src/main/java/com/beeasy/hzbpm/App.java
+++ hz-bpm/src/main/java/com/beeasy/hzbpm/App.java
@@ -2,9 +2,14 @@ package com.beeasy.hzbpm;
 
 import com.github.llyb120.nami.core.Nami;
 
+import java.util.concurrent.CountDownLatch;
+
+import static com.github.llyb120.nami.ext.beetlsql.BeetlSql.sqlManager;
+import static com.beeasy.hzbpm.service.MongoService.db;
+
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String config = null;
         if(args.length > 0){
             for (int i = 0; i < args.length; i++) {
@@ -15,10 +20,14 @@ public class App {
         }
 
         if (config == null) {
-            Nami.start();
-        } else {
-            Nami.start(config, false);
+            config = "./nami.conf";
         }
+        Nami.start(config, true);
+        sqlManager.getBeetl();
+        db.getName();
+
+        CountDownLatch cd = new CountDownLatch(1);
+        cd.await();
     }
 }
 
