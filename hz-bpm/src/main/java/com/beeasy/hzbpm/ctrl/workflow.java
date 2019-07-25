19759a70c2cf61363b1417172c77e409dd76409e
your_account <503762718@qq.com>
2019-07-25 17:07:14 +0800

    工作流设置

--- hz-bpm/src/main/java/com/beeasy/hzbpm/ctrl/workflow.java
+++ hz-bpm/src/main/java/com/beeasy/hzbpm/ctrl/workflow.java
@@ -13,6 +13,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.UpdateOptions;
+import org.beetl.sql.core.SQLReady;
 import org.bson.BsonArray;
 import org.bson.Document;
 import org.bson.conversions.Bson;
@@ -22,6 +23,7 @@ import javax.xml.crypto.Data;
 import java.util.*;
 
 import static com.beeasy.hzbpm.service.MongoService.db;
+import static com.github.llyb120.nami.ext.beetlsql.BeetlSql.sqlManager;
 import static com.github.llyb120.nami.json.Json.a;
 import static com.github.llyb120.nami.json.Json.o;
 
@@ -55,12 +57,32 @@ public class workflow {
     }
 
     /**
-     * 验证发起权限
+     * 发起人所需填写表单
      * @param formId
      * @return
      */
-    public Result verifica(String formId){
+    public Result startExt(String formId){
+        Long uid = Auth.getUid();
+        Arr list = a();
 
+        Object startNode = startNode();
+
+        Set<Long> uidSet = startList(startNode);
+
+        if(!uidSet.contains(uid)){
+            return Result.error("没有权限发起流程！");
+        }
+        Object ext = ((Document) startNode).get("ext");
+        return Result.ok(ext);
+
+    }
+
+    /**
+     * 获取最终生成的流程文件
+     * @param formId 表单ID
+     * @return
+     */
+    private Arr bpmWorkflow(String formId) {
         Arr list = a();
         MongoCollection<Document> collection = db.getCollection("workflow");
         collection.find(new Document(){{
@@ -70,26 +92,15 @@ public class workflow {
                 .forEachRemaining(d -> {
                     String id = d.getObjectId("_id").toHexString();
                     d.put("_id", id);
-                    List<Object> data = (List<Object>) d.get("data");
-                    for(Object li : data){
-                        Document a = (Document) li;
-                        String start = (String) a.get("start");
-                        Map<String, Object> nodeMap = (Map<String, Object>) a.get("node");
-                        Object startNode = nodeMap.get(start);
+                    list.add(d.get("data"));
 
-
-                        System.out.println(startNode);
-                        // TODO 判断当前登录人是否在发起节点权限设置中
-
-                        list.add(((Document) startNode).get("taskId"));
-
-                    }
                 });
-
-
-        return Result.ok(list);
+        return list;
     }
 
+    /**
+     * 发起节点信息
+     */
     private Object startNode(){
         Arr list = a();
         MongoCollection<Document> collection = db.getCollection("workflow");
@@ -114,15 +125,82 @@ public class workflow {
     }
 
     /**
+     * 发起人名单
+     */
+    private Set<Long> startList(Object startNode){
+
+        Set<Long> uidSet = new HashSet<Long>();
+        List<Long> uidList = (List<Long>) ((Document) startNode).get("uids");
+        List<Long> qidList = (List<Long>) ((Document) startNode).get("qids");
+        List<Long> didList = (List<Long>) ((Document) startNode).get("dids");
+        List<Long> ridList = (List<Long>) ((Document) startNode).get("rids");
+        uidSet.addAll(uidList);
+        uidSet.addAll(qidList);
+        uidSet.addAll(didList);
+        uidSet.addAll(ridList);
+        return uidSet;
+    }
+
+    /**
      * 提交
      */
-    public void submitIns(Obj bady){
+    public Result submitIns(Obj bady){
         Long uid = Auth.getUid();
         String formId = bady.s("formId");
+        Arr bpmWorkflow = bpmWorkflow(formId);
+
+        for(Object li : bpmWorkflow){
+            Document a = (Document) li;
+            String start = (String) a.get("start");
+            Map<String, Object> nodeMap = (Map<String, Object>) a.get("node");
+            Object startNode = nodeMap.get(start);
+
+        }
 
         Object startNode = startNode();
+        Set<Long> uidSet = new HashSet<>();
+
+        List<Long> uidList = (List<Long>) ((Document) startNode).get("uids");
+        List<String> qidList = (List<String>) ((Document) startNode).get("qids");
+        List<String> didList = (List<String>) ((Document) startNode).get("dids");
+        List<String> ridList = (List<String>) ((Document) startNode).get("rids");
 
 
+        // 岗位
+        List<Long> qInfo = sqlManager.execute(new SQLReady("select id from t_user u where exists(select 1 from t_user_org where uid = u.id and oid in "+qidList+")"),Long.class);
+        // 角色
+        List<Long> rInfo = sqlManager.execute(new SQLReady("select id from t_user u where exists(select 1 from t_user_org where uid = u.id and oid in "+ridList+")"),Long.class);
+        // 部门
+        List<Long> dInfo = sqlManager.execute(new SQLReady("select uid from t_user_org where oid  in (select id from t_org where PARENT_ID in "+didList+")"),Long.class);
+
+        uidSet.addAll(uidList);
+        uidSet.addAll(rInfo);
+        uidSet.addAll(dInfo);
+        uidSet.addAll(qInfo);
+
+
+        if(!uidSet.contains(uid)){
+            return Result.error("没有权限发起流程！");
+        }
+            //任务所属部门
+//        List<Obj> userInfo = sqlManager.execute(new SQLReady("select u.id,u.true_name as name,o.oid from t_user u inner join t_user_org o on o.uid=u.id and uid="+uid),Obj.class);
+//
+//        List<String> didList = (List<String>) ((Document) startNode).get("dids");
+//        String userName = "";
+//        List<String> orgSet = new ArrayList<>();
+//        if(userInfo.size()>0){
+//            userName = userInfo.get(0).s("name");
+//            for(Obj list : userInfo){
+//                orgSet.add(list.s("oid"));
+//            }
+//        }
+
+
+
+
+
+
+        return Result.ok();
     }
 
 }
